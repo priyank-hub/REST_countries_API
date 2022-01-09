fdabb851b8acc3fafe5b03b5b0789f574289540e
Priyank <patelpriyank177@gmail.com>
2022-01-09 13:50:47 -0500

    UI for country cards

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -1,27 +1,23 @@
 <template>
-  <div>
-    <div class="container mt-5">
+  <div class="bg-light">
+    <div id="countries" class="container mt-5">
 
         <!-- filters -->
         <div class="row justify-content-between align-items-center">
           <div class="col-12 col-md-5">
             <v-form>
               <v-container>
-                <v-row>
-                  <v-col 
-                  cols="12">
-                    <v-text-field
-                      label="Search for a country..."
-                      solo
-                      v-model="search"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
+                <v-text-field
+                  placeholder="Search for a country..."
+                  solo
+                  v-model="search"
+                  style="font-size: 14px"
+                ></v-text-field>
               </v-container>
             </v-form>
           </div>
 
-          <div class="col-12 col-md-4">
+          <div class="col-12 col-md-3">
             <div class="text-center">
               <!-- <v-menu offset-y>
                 <template v-slot:activator="{ on, attrs }">
@@ -67,11 +63,63 @@
         </div>
         
         <!-- countries -->
-        <div>
-          <h3>All countriesssss</h3>
-          <div v-for="(country, index) in filteredCountries" :key="index">
-            <img :src="country.flags.svg" height="50" alt="">
-            {{ country.name.common }} <br><br>
+        <div v-if="fetching">
+          <div class="row justify-content-center">
+            <div class="col-12 col-md-3 col-lg-4" v-for="i in sample" :key="i+501">
+              <div class="m-2">
+                <v-skeleton-loader
+                  class=""
+                  max-width="344"
+                  type="card"
+                ></v-skeleton-loader>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-else class="row justify-content-center">
+          <div class="col-12 col-md-3 col-lg-4" v-for="(country, index) in filteredCountries" :key="index">
+            <div class="m-2">
+              <v-card
+                class=""
+                max-width="344"              
+              >
+                <v-img
+                  :src="country.flags.svg"
+                  height="200px"
+                ></v-img>
+
+                <v-card-title style="font-weight: 700; font-size: 16px">
+                  <span v-if="country.name.common.length > 17">
+                    {{ country.name.common.substring(0,15) }}...
+                  </span>
+                  <span v-else>
+                    {{ country.name.common }}
+                  </span>
+                </v-card-title>
+
+                <v-card-text class="text-left">
+                  <div>
+                    <b>Population: </b>
+                    <span>
+                      {{ country.population.toLocaleString() }}
+                    </span>
+                  </div>
+                  <div>
+                    <b>Region: </b>
+                    <span>
+                      {{ country.region }}
+                    </span>
+                  </div>
+                  <div>
+                    <b>Capital: </b>
+                    <span v-for="capital in country.capital" :key="capital">
+                      {{ capital }}
+                    </span>
+                  </div>
+                </v-card-text>
+              </v-card>          
+            </div>
           </div>
         </div>
     </div>
@@ -89,6 +137,8 @@ export default {
   data() {
     return {
         countries: [],
+        sample: [],
+        fetching: false,
         search: '',
         region: '',
         items: [
@@ -119,11 +169,16 @@ export default {
     },
   },
   async mounted() {
+    for(let i=0; i<100; i++){
+      this.sample.push(i);
+    }
+    this.fetching = true;
     await axios.get('https://restcountries.com/v3.1/all')
     .then(res => {
       console.log('response', res)
       if (res.status == 200) {
         this.countries = res.data
+        this.fetching = false;
       } 
       else {
         console.log('Opps error');
@@ -143,5 +198,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#countries{
+  font-family: Outfit, Helvetica, Arial, sans-serif !important;
+}
 </style>
