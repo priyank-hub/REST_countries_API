f5ecc52f5593196426a5bc687abcf5f562ef03d4
priyank-hub <54325230+priyank-hub@users.noreply.github.com>
2022-01-09 11:01:54 -0500

    second commit

---
+++ src/components/Countries.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="container mt-5">
+
+        <!-- filters -->
+        <div class="row">
+            <b-form>
+                <b-form-input v-model="search" class="border-0" type="text" placeholder="Search for a country..." style="box-shadow: 0px 0px 13px -15px #000000">
+
+                </b-form-input>
+            </b-form>
+        </div>
+        
+        <!-- countries -->
+        <div>
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Countries',
+  props: {
+    
+  },
+  data() {
+      return {
+          search: '',
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    
+    <!-- appbar -->
+    <nav class="bg-white p-3 border-bottom" style="box-shadow: 0px 5px 20px -20px #000000">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div>
+            <span class="" style="font-size: 20px; font-weight: 700; color: black">
+              Where in the world?
+            </span>
+          </div>
+
+          <div style="color: black">
+            <i class="far fa-moon"></i>
+            <span>
+              Dark Mode
+            </span>
+          </div>
+        </div>
+      </div>      
+    </nav>
+
+    <!-- list of all countries -->
+    <Countries />
+
+  </div>
+</template>
+
+<script>
+import Countries from './Countries'
+export default {
+  name: 'Main',
+  props: {
+    
+  },
+  components: {
+    Countries,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
